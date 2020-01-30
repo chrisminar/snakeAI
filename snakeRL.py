4f5da89d6a788d6b1349d50f7dec9756beb80ff3
chrisminar <majorminar137@gmail.com>
2020-01-30 08:20:59 -0800

    fleshed out dataframe sudocode

--- snakeRL.py
+++ snakeRL.py
@@ -11,88 +11,22 @@
 #b_ 1600
 #c_ 500000
 
-# sweet things to do
-  # features
-    # do network pruning
-    # automated unit testing?
-    # track/understand what the neural network is doing
-    # gui
-    # pause/play 
-  # design processes
-    # make all the data handled with pandas
-    # how can parallelism be abused to make this faster?
-    # develop with unit testing as I go
-  # evaluation
-    # sweep the number of blocks vs performance
-    # do something with computational power vs performance
-
-# domain knowledge -- what should the ai know?
-  # mcts should attempt to not go into body or wall
-
-# WORK BLOCKS
-
-# learning
-  # how to pandas
-  # how to do automatic unit testing with python/git/VS
-  # mcts
-
-#YOU ARE HERE. FIGURE OUT MORE SPECIFIC SIZES FOR EVERYTHING AND FLUSH OUT DATATYPES
-#THEN START WRITING SUDO CODE
-# data inputs for each step
-#neural network -- training
-  # last 20000 games of self play
-# neural network -- feedforward
-  # game state
-#mcts evaluator
-  # best neural network
-  # current neural network
-#mcts
-  # game state
-  # neural network
-#self play
-  # best neural network
-
-# data outputs for each step
-#neural network -- training
-  # newest neural network
-# neural network -- feedforward
-  # vector of move probabilities -p
-  # predicted value -v
-# mcts evaluator
-  # 400 game outputs
-  # mean score over 400 games
-# mcts
-  # new move
-# self play
-  # 2000 game outputs
-
-# data tracking
-# neural network -- training
-  # time -- minibatch
-  # time -- generation
-# neural network -- feedforward
-  
-# mcts evaluator
-  # time -- game
-  # time -- generation
-# mcts
-  # time -- tree generation
-  # time -- single ffnn
-# self play
-  # time -- generation
+# evaluation
+  # sweep the number of blocks vs performance
+  # do something with computational power vs performance
 
 # neural network trainer -- update neural network parameters theta_i -using recent self play data
   # neural network
     # tensorflow blocks
     # BLOCK
-    # convolution 256 filters, 3x3 patch, stride 1
+    # convolution 64 filters, 3x3 patch, stride 1
     # batch norm
     # relu
-    # convolution 256 filters, 3x3 patch, stride 1
+    # convolution 64 filters, 3x3 patch, stride 1
     # batch norm
     # skip connection that adds block input
     # relu
-    # convolution 256 filters, 3x3 patch, stride 1
+    # convolution 64 filters, 3x3 patch, stride 1
     # batch norm
     # skip connection that adds block input
     # relu
@@ -102,7 +36,7 @@
     # convolution 1 filter, 1x1 patch, stride 1
     # batch norm
     # relu
-    # fully connceced to hidden layer size 256
+    # fully connceced to hidden layer size 64
     # relu
     # fully connected linear layer to scaler
     # tanh outputing a scalar in range -1 1
@@ -150,3 +84,140 @@
 # gui
 # pause/play 
 # ORDER
+
+
+# domain knowledge -- what should the ai know?
+  # mcts should attempt to not go into body or wall
+
+# WORK BLOCKS
+
+# learning
+  # how to pandas
+  # how to do automatic unit testing with python/git/VS
+  # mcts
+
+# YOU ARE HERE. FIGURE OUT MORE SPECIFIC SIZES FOR MCST
+# FIGURE OUT THE DATA STRUCTS TO HOLD ALL OF THIS INFORMATION
+  # dataframes -- done
+  # working lists/arrays -- todo
+# WHAT IS THE PURPOSE OF 2 HEADS?
+# what is the purpose of having distinct evaluations and self play?
+# WRITE SUDO CODE
+# WRITE SKELETAL ARCHITECHTURE
+#game state
+  # array gridX x gridY
+    # food 2
+    # empty 1
+    # body -1
+#neural net
+  # 2 blocks 64 filters each
+  # 1 head policy
+  # 1 head value
+#self play game
+  # game state for each turn
+  # end score
+  # iteration of a_theta_i that made the game
+#self play list
+  #list of self play games
+#mcts step
+
+#new move
+  #direction enumerator
+#vector of move probabilities -p
+  # 4 array with probabilities for up,right,down,left
+#vector predicted value -v
+  # 4 array with predicted value for up,right,down,left
+
+# data inputs for each step
+#neural network -- training
+  # last 20000 games of self play
+# neural network -- feedforward
+  # game state
+#mcts evaluator
+  # best neural network
+  # current neural network
+#mcts
+  # game state
+  # neural network
+#self play
+  # best neural network
+
+# data outputs for each step
+#neural network -- training
+  # newest neural network
+# neural network -- feedforward
+  # vector of move probabilities -p
+  # predicted value -v
+# mcts evaluator
+  # 400 game outputs
+  # mean score over 400 games
+# mcts
+  # new move
+# self play
+  # 2000 game outputs
+
+# data tracking -- put this in pandas datastructs
+# generation
+  # self play
+    # 2000 games
+      # time
+      # score
+    # generation time
+    # mean score
+  # mcts evaluator
+    # 400 games
+      # time of game
+      # score of game
+      # number of moves taken
+      # total time for mcts tree generation
+      # total time for ffnn
+      # mean time for mcts tree generation
+      # mean time for ffnn
+    # mean score
+    # generation time
+  # training -- ammount of training data per generation will not be consistent due to different games lengths so don't track mini-batch-wise statistics
+    # 20000 training games
+    # total training time
+    # mean training time per mini batch
+  
+  # dataframes:
+  # 1 self play detail
+    # columns: time(float),score(int)
+    # index1: generation(list[0-?])
+    # index2: games(list[0-1999])
+  # 2 self play broad
+    # columns: mean score(float), time(float)
+    # index: generation(list[0-?])
+  # 3 evaluator detailed
+    # columns: 
+      # time of game(float)
+      # score of game(int)
+      # #moves(int)
+      # total time for mcts tree generation(float)
+      # total time for ffnn(float)
+      # mean time for mcts tree generation (float)
+      # mean time for ffnn (float)
+    # index:
+      #1: generation(list[0-?])
+      #2: games(list[0-399])
+  # 4 evaluator broad
+    #columns:
+      #mean score(float)
+      #generation time (float)
+    #index generation(list[0-?])
+  #5 training
+    #columns: 
+      # generation time (float)
+      # # minibatches(int)
+      # mean training time per mini batch(float)
+    #index:
+      #generation(list[0-?])
+
+
+
+
+
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
