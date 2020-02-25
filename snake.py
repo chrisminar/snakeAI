a5844a7b5b2fd75027f68519587fef3b34c6dbed
chrisminar <majorminar137@gmail.com>
2020-02-25 07:29:16 -0800

    snake class extended

--- snake.py
+++ snake.py
@@ -10,7 +10,7 @@ buttons = {0: "up",
            4: "none"}
 
 class snake():
-  def __init__(self, isHuman = True, sizeX=20, sizeY=20):
+  def __init__(self, sizeX=20, sizeY=20):
     #grid
     self.sizeX = sizeX                       #width of grid
     self.sizeY = sizeY                       #height of grid
@@ -29,10 +29,6 @@ class snake():
     self.gameoverThreshold = -self.sizeX*self.sizeY*2
     self.moveThreshold = self.sizeX*self.sizeY*2
     self.moves = 0
-    currentstate = random.getstate()         #set random seed without ruining the other seed
-    random.seed(0)
-    self.randomState = random.getstate()
-    random.setstate(currentstate)
 
     #input
     dir = random.randint(0,3)
@@ -51,65 +47,6 @@ class snake():
 
     #gamestate
     self.gameover = False
-    self.isHuman = isHuman
-    if self.isHuman:
-      self.humanGameInit()
-    else:
-      self.aiGameInit()
-
-  def humanGameInit(self):
-    #init pygame
-    pg.init()
-    self.DISPLAY = pg.display.set_mode((self.sizeX*21,self.sizeY*21),0,32)#init display
-    pg.display.set_caption('Snake')
-    self.DISPLAY.fill((255,255,255)) #fill with white
-    self.foodX,self.foodY = self.spawn_food()
-    self.grid[self.foodX][self.foodY] = -2   #set food on grid
-    self.displayState()
-
-  def aiGameInit(self):
-    self.foodX,self.foodY = self.spawn_food()
-    self.grid[self.foodX][self.foodY] = -2   #set food on grid
-
-  def humanRun(self):
-    while (self.gameover == False):
-      events = pg.event.get()
-      for event in events:
-        if event.type==pg.KEYDOWN:
-          if event.key == pg.K_LEFT:
-            self.runSingle(-1, 0)
-            #print('left')
-          elif event.key == pg.K_RIGHT:
-            self.runSingle(1,0)
-            #print('right')
-          elif event.key == pg.K_DOWN:
-            self.runSingle(0,1)
-            #print('down')
-          elif event.key == pg.K_UP:
-            self.runSingle(0,-1)
-            #print('up')
-          self.displayState()
-    return self.score
-
-  def aiRunStep(self, dir: str): 
-    if not self.gameover:
-      self.moves+=1
-      if dir == buttons[3]: #left
-        self.runSingle(-1, 0)
-      elif dir == buttons[1]: #right
-        self.runSingle(1,0)
-      elif dir == buttons[2]: #down
-        self.runSingle(0,1)
-      elif dir == buttons[0]: #up
-        self.runSingle(0,-1)
-      elif dir == buttons[4]: #no input
-        self.runSingle(self.Xdir,self.Ydir)
-      else: #invalid direction = no input
-        self.runSingle(self.Xdir,self.Ydir)
-      return
-    else: #game is over
-      return
-
 
   def runSingle(self, xdir, ydir):
     self.Xdir = xdir
@@ -216,93 +153,3 @@ class snake():
       return True
 
     return False
-
-class snake_test():
-  def __init__(self):
-    self.snake = snake(8,8)
-    self.count = 0
-    self.passed = 0
-    self.failed = 0
-
-  def run_tests(self):
-    self.test_spawn_food()
-    self.test_check_game_over()
-    print('passed',self.passed)
-    print('failed',self.failed)
-
-  def test_spawn_food(self):
-    print('testing spawn food')
-    self.snake.grid = np.zeros((8,8))
-    for k in range(0,7):
-      self.snake.length = 0
-      #make grid
-      for i in range(8):
-        for j in range(8):
-          if (i == k) or (i==k+1):
-            self.snake.grid[i][j] = self.snake.length
-            self.snake.length+=1
-          else:
-            self.snake.grid[i][j] = -1
-      #test
-      (x,y) = self.snake.spawn_food()
-      self.snake.grid[x][y] = -2
-      if (x == k) or (x==k+1):
-        print(self.snake.grid)
-        print('food location',x,y)
-        print (self.count,'failed')
-        self.failed += 1
-      else:
-        #print (self.count,'passed')
-        self.passed += 1
-      self.count += 1
-  
-  def test_check_game_over(self):
-    print('testing check game over')
-    self.snake.length = 0
-    #set grid
-    for i in range(8):
-        for j in range(8):
-          if (i == 4) or (i == 5):
-            self.snake.grid[i][j] = self.snake.length
-            self.snake.length+=1
-          else:
-            self.snake.grid[i][j] = -1
-    #sweep through possibilities
-    test = False
-    for i in range(-1,10):
-      for j in range(-1,10):
-        self.snake.X = i
-        self.snake.Y = j
-        self.snake.gameover = False
-        test = self.snake.check_game_over()
-        #check out of bounds
-        if (i == -1) or (i == 9) or (j == -1) or (j == 9):
-          if test == True:
-            #print (self.count,'passed')
-            self.passed += 1
-          else:
-            print (self.count,'failed')
-            self.failed += 1
-        #check for overlap with snake
-        elif (i == 4) or (i == 5):
-          if test == True:
-            #print (self.count,'passed')
-            self.passed += 1
-          else:
-            print (self.count,'failed')
-            self.failed += 1
-        else:
-          if test == False:
-            #print (self.count,'passed')
-            self.passed += 1
-          else:
-            print (self.count,'failed')
-            self.failed += 1
-        self.count += 1
-
-if __name__ == "__main__":
-  #s = snake_test()
-  #s.run_tests()
-  s = snake()
-  s.run()
-  print(s.score)
---
+++ trainSnakeRL.py
@@ -0,0 +1,194 @@
+#glossary
+#p: vector of move probabilities (likelyhood of choosing a given move)
+#v: predictied victor of the game
+#s: board state
+#pi: mcts probabilities of playing each move
+#f_theta: neural network
+#z game winner
+#a_ 25000
+#b_ 1600
+#c_ 500000
+
+# evaluation
+  # sweep the number of blocks vs performance
+  # do something with computational power vs performance
+
+# neural network trainer -- update neural network parameters theta_i -using recent self play data
+  # using the last x games, train the network
+    #how do positive and negative examples work?
+    #outputs of neural network?
+    #inputs to neural network?
+  #neural network (p,v) = f_theta(s) is adjusted to minimse the error between the predicted value v and the self play winner z, and to maximise the similarities between p and pi.
+  #loss function l=(z-v)^2-pi^T log(p)+c||theta||^2
+  #c = constant = 1e-4
+
+# mcts evaluator -- alpha go players a_theta_i are evaluated and best performing generates new self play data
+  # game terminates when staleness threshold is passed, or when score exceeds a certain length
+  # MCTS -- monte carlo tree search
+    # inputs to mcts?
+    # outputs to mcts?
+    # data storage/tracking?
+  # inputs to mcts evaluator
+  # outputs to mcts evaluator
+  # checkpoint creator -- save best theta
+  # checkpoint loader -- load best theta
+  # checkpoint every 1000 steps
+  # evaluation consists of 400 games
+  #if checkpoint_t scores 1.2x> checkpoint_(i_best), it is the new baseline and used to generate data
+  #mcts
+    # store:
+      # node
+        # state
+        # Prior probability P (s,a)
+        # vist count N (s,a)
+        # action value Q(s,a)
+        # children
+        # parent
+    # selection -- starts from root and selects children nodes until a leaf is found.
+      #maximise confidence bound to traverse the tree
+      #Q(s,a) + U(s,a)
+      #U(s,a) = P(s,a)/(1+N(s,a))
+    # expansion -- make a new node from leaf using output from neural net f_theta
+    # simulation -- play out a game somehow and determine a winner
+      # P(s'), V(s') = f_theta(s') --- probability and prediction from new state with neural net f_theta
+    # backpropagation -- update the tree using the winner of simulation
+      #each node (s,a) is traversed to update:
+        #N(s,a) += 1
+        #Q(s,a) = 1/N(s,a) Sum_s'|s,a->s' V(s')
+          #s,a->s' indicates a simulation reached s' after taking move a from position s (sum the number of times this node was chosen)
+# self play
+    #for the first 30 moves the temperature is set to 1, afterwards it is set to 0
+
+# do network pruning
+# automated unit testing
+# track/understand what the neural network is doing
+# gui
+# pause/play 
+
+# domain knowledge -- what should the ai know?
+  # mcts should attempt to not go into body or wall
+
+# WORK BLOCKS
+# WRITE CODE
+  # training #YOU ARE HERE
+    # write function to concatenate output from all games
+    # write function to kick out old games from the gamelist
+      #something like:
+        #last = gameid.max()
+        #idx = np.argwhere(gameid>(last-20000))
+        #scores = scores(idx)
+        #states = states(idx)
+    #STOP, TEST THESE FUNCTIONS
+    # train nn
+  #evaluator
+# add data tracking
+# figureout how mcts works
+
+# self play game
+  # game state for each turn
+  # end score
+  # iteration of a_theta_i that made the game
+# self play list
+  # list of self play games
+# mcts
+  # state
+  # Prior probability P (s,a)
+  # vist count N (s,a)
+  # action value Q(s,a)
+  # children
+  # parent
+
+# data tracking -- put this in pandas datastructs
+# generation
+  # self play
+    # 2000 games
+      # time
+      # score
+    # generation time
+    # mean score
+  # mcts evaluator
+    # 400 games
+      # time of game
+      # score of game
+      # number of moves taken
+      # total time for mcts tree generation
+      # total time for ffnn
+      # mean time for mcts tree generation
+      # mean time for ffnn
+    # mean score
+    # generation time
+  # training -- ammount of training data per generation will not be consistent due to different games lengths so don't track mini-batch-wise statistics
+    # 20000 training games
+    # total training time
+    # mean training time per mini batch
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import typing
+from typing import List,Tuple
+from dataTrack import dataTrack
+from mcts import mcts
+from mcts import mcts_node
+from neuralNet import neural_network
+from neuralNet import nn_out
+from game_state import game_state
+from globalVar import globe
+from selfPlayClass import selfPlayClass
+
+
+class snakeRL():
+  def __init__(self):
+    self.tracker = dataTrack()
+    self.gameList = []
+    self.nnList = []
+    pass
+
+
+  def train(self):
+    generation = 0
+    while 1:
+      self.selfPlay(generation)
+      #todo, need a function that kicks out the lowest scored games from the game list
+      self.networkTrainer()
+      self.mcts_evaluator()
+      generation += 1
+
+  #operates on:
+    #best neural network
+  #outputs:
+    #2000 game outputs
+  def selfPlay(self, nn:neural_network, generation:int):
+    spc = selfPlayClass(nn)
+    spc.playGames(generation)
+    self.gameList += spc.gamelist
+
+  #operates on:
+    # last 20000 games of self play
+  #outputs:
+    #new neural network
+  def networkTrainer(self):
+    pass
+
+   #operates on: 
+    #best neural network
+    #current neural network
+  #outputs:
+    #400 games (with gamestate)
+    #mean score of 400 games
+  def mcts_evaluator(self):
+    pass
+
+#######################
+## network functions ##
+#######################
+#load checkpoint
+#train network
+#save checkpoint
+#add nn to nn list
+#add statistics
+
+####################
+## mcts evaluator ##
+####################
+#add statistics
